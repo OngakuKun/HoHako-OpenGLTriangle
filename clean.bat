c648d3067f1fadf10317b7deecd07ffa7c8e9379
OngakuKun <marcellehmkuhl@outlook.de>
2022-08-03 23:30:32 +0200

    Add %OUTDIR%

--- clean.bat
+++ clean.bat
@@ -3,6 +3,7 @@ pushd "%~dp0"
 
 REM ===     HELPER VARIABLES     ===
     set ASSAMBLY=HelloWorld
+    set OUTDIR=bin\
 
 REM ===   CLEAN FILES AND DIRS   ===
     del /s %OUTDIR%*.obj %OUTDIR%*.exe %OUTDIR%*.pdb %OUTDIR%*.ilk %OUTDIR%*.lib %OUTDIR%*.dll
